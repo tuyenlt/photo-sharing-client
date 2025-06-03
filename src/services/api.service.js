b521e0a03c97d99e04dcea5bbfae5e5161a433eb
Lê Trọng Tuyên <85683857+tuyenlt@users.noreply.github.com>
2025-06-03 14:45:36 +0000

    update

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000/api";
+const API_BASE_URL =
+  import.meta.env.VITE_API_URL || "https://w57p2f-3000.csb.app/api";
 
 const api = axios.create({
-	baseURL: API_BASE_URL,
-	headers: {
-		"Content-Type": "application/json",
-	},
+  baseURL: API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
-
 
 export default api;
